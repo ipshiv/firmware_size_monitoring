be771580309600d0336a28f641719e637db4da5b
Ivan Shivarev <ivan.shivarev@huld.io>
2022-08-01 16:44:00 +0300

    test: zephyr: add new functions to main

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from StatsParser import StatsParserZephyr
 
 def main():
     print("I'm main mock!")
-    sparser = StatsParserZephyr("output.txt", "status.txt")
+    sparser = StatsParserZephyr("test_input/output.txt", "test_input/status.txt")
     sparser.printStatsWarnings()
     sparser.printStatsMemory()
     sparser.printStatsTable()
+    sparser.printDiffwarnings("templates/STATS.md")
+    sparser.printDiffLibraries("templates/STATS.md")
 
 
 if __name__ == '__main__':
