220976c486c7253f4e91993e009e47de7c91340e
Ivan Shivarev <ivan.shivarev@huld.io>
2022-08-03 15:18:51 +0300

    feat: script: store output data

--- run_on_dev_folder.py
+++ run_on_dev_folder.py
@@ -6,18 +6,35 @@ from StatsParser import StatsParserZephyr
 
 
 def main(path):
+    output_data = {
+        "current_build": {
+            "build_sha": "",
+            "warnings": 0,
+            "build_stats_text": "",
+            "build_stats_row": "",
+            "lib_stats_markdown": "",
+        },
+        "retrospective": {
+            "warnings_markdown": {"size": 0, "table_rows": []},
+            "stats": {"size": 0, "table_rows": []},
+        },
+    }
     root_path = path
     res_path = os.path.join(path, "resources")
     repo = Repo(root_path)
-    head = repo.heads[0]
-    print(head.name)
-    print(head.commit.hexsha)
+    output_data["current_build"]["build_sha"] = "#" + repo.heads[0].commit.hexsha[:7]
     sparser = StatsParserZephyr(
         os.path.join(root_path, "output.txt"), os.path.join(root_path, "status.txt")
     )
-    sparser.printStatsWarnings()
-    sparser.printStatsMemory()
-    sparser.printStatsTable()
+    output_data["current_build"]["warnings"] = sparser.countWarnings
+    output_data["current_build"]["build_stats_text"] = sparser.buildStats
+    output_data["current_build"]["build_stats_row"] = sparser.buildStatsRow
+    output_data["current_build"]["build_stats_row"][0] = output_data["current_build"][
+        "build_sha"
+    ]
+    output_data["current_build"]["lib_stats_markdown"] = sparser.generateStatsTable()
+
+    print(output_data)
 
 
 if __name__ == "__main__":
