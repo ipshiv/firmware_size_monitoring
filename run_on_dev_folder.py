2ba08926c12103568443203f7bcc2851ab40d37c
Ivan Shivarev <ivan.shivarev@huld.io>
2022-08-04 16:28:11 +0300

    ref: test: res_path as wariable. DRY

--- run_on_dev_folder.py
+++ run_on_dev_folder.py
@@ -21,7 +21,7 @@ def main(path):
         },
     }
     root_path = path
-    res_path = os.path.join(path, "resources")
+    res_path = os.path.join(path, "resources/STATS.md")
     repo = Repo(root_path)
     output_data["current_build"]["build_sha"] = "#" + repo.heads[0].commit.hexsha[:7]
     sparser = StatsParserZephyr(
@@ -30,17 +30,14 @@ def main(path):
     )
     output_data["current_build"]["warnings"] = sparser.countWarnings
     output_data["current_build"]["build_stats_text"] = sparser.buildStats
-    output_data["current_build"]["build_stats_row"] = sparser.buildStatsRow
-    output_data["current_build"]["build_stats_row"][0] = output_data["current_build"][
-        "build_sha"
-    ]
-    if not os.access(os.path.join(root_path, "resources/STATS.md"), os.R_OK):
+
+    if not os.access(res_path, os.R_OK):
         output_data["current_build"][
             "lib_stats_markdown"
         ] = sparser.generateLibsTable()
     else:
         output_data["current_build"]["lib_stats_markdown"] = sparser.generateDiffTable(
-            os.path.join(root_path, "resources/STATS.md")
+            os.path.join(res_path)
         )
     
     output_data["retrospective"]["warnings_markdown"] = sparser.generateWarningsTable(os.path.join(res_path))
